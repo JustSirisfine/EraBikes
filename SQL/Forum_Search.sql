9466c1ef39dd9acf00d6876f4648c277e5545f95
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 14:03:00 -0500

    Update Forum_Search.sql

--- SQL/Forum_Search.sql
+++ SQL/Forum_Search.sql
@@ -19,27 +19,27 @@ BEGIN
 
 	DECLARE @Rows int = @PageIndex * @PageSize
 
-			SELECT f.[Id]
-			   ,f.[Name]
-			   ,f.[Description]
-			   ,f.[ForumCategoryId]
-			   ,fc.[Name]
-			   ,f.[IsPrivate]
-			   ,f.[IsClosed]
-			   ,f.[DateCreated]
-			   ,f.[DateAdded] 
-			   ,dbo.fn_GetUserJSON(f.[CreatedBy]) as [CreatedBy]
+	SELECT f.[Id]
+	   ,f.[Name]
+	   ,f.[Description]
+	   ,f.[ForumCategoryId]
+	   ,fc.[Name]
+	   ,f.[IsPrivate]
+	   ,f.[IsClosed]
+	   ,f.[DateCreated]
+	   ,f.[DateAdded] 
+	   ,dbo.fn_GetUserJSON(f.[CreatedBy]) as [CreatedBy]
 
-			   ,TotalCount = COUNT (1) over ()
+	   ,TotalCount = COUNT (1) over ()
 
-		  FROM [dbo].[Forum] as f
-		  Inner Join [dbo].[ForumCategories] as fc
-		  ON f.ForumCategoryId = fc.Id
-		  WHERE (f.[Name] LIKE '%' + @Query + '%' OR f.[Description] LIKE '%' + @Query + '%' )
+	  FROM [dbo].[Forum] as f
+	  Inner Join [dbo].[ForumCategories] as fc
+	  ON f.ForumCategoryId = fc.Id
+	  WHERE (f.[Name] LIKE '%' + @Query + '%' OR f.[Description] LIKE '%' + @Query + '%' )
 
-		  ORDER BY f.[DateCreated] desc
+	  ORDER BY f.[DateCreated] desc
 
-		  OFFSET @Rows ROWS
-		  FETCH NEXT @PageSize ROWS only
+	  OFFSET @Rows ROWS
+	  FETCH NEXT @PageSize ROWS only
 
 END
