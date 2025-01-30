adea8e1b8feeb4727cbf18cb09704e475da93c38
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:28:22 -0500

    Update Forum_SelectByIdV2.sql

--- SQL/Forum_SelectByIdV2.sql
+++ SQL/Forum_SelectByIdV2.sql
@@ -1,5 +1,5 @@
 ALTER PROCEDURE [dbo].[Forum_SelectByIdV2]
-				@Id INT
+		@Id INT
 
 /* ---------------- TEST CODE ------------
 Declare @Id INT = 4
@@ -13,34 +13,34 @@ as
 
 BEGIN
 
-    		SELECT 
-    			f.[Id],
-    			f.[Name],
-    			f.[Description],
-    			f.[ForumCategoryId],
-    			fc.[Name] AS ForumCategoryName,
-    			f.[IsPrivate],
-    			f.[IsClosed],
-    			f.[DateCreated],
-    			f.[DateAdded],
-    			[dbo].[fn_GetUserJSON](f.CreatedBy) AS AuthorId,
-    		
-    			(
-    				SELECT 
-    					fa.[Id] AS AnswerId,
-    					fa.[AnswerTitle] AS AnswerTitle,
-    					fa.[Description] AS AnswerDescription,		
-    					[dbo].[fn_GetUserJSON](fa.AuthorId) AS AnswersAuthorId,
-    					fa.[DateCreated] AS AnswerDateCreated,
-    					fa.[ThreadId] AS AnswerThreadId
-    				FROM [dbo].[ForumAnswers] AS fa
-    				WHERE fa.[ThreadId] = f.[Id]
-    				FOR JSON AUTO
-    			) AS Answers 
-    
-    		FROM [dbo].[Forum] AS f
-    		INNER JOIN [dbo].[ForumCategories] AS fc
-    		ON f.[ForumCategoryId] = fc.[Id]
-    		WHERE f.[Id] = @Id;
+	SELECT 
+		f.[Id],
+		f.[Name],
+		f.[Description],
+		f.[ForumCategoryId],
+		fc.[Name] AS ForumCategoryName,
+		f.[IsPrivate],
+		f.[IsClosed],
+		f.[DateCreated],
+		f.[DateAdded],
+		[dbo].[fn_GetUserJSON](f.CreatedBy) AS AuthorId,
+	
+		(
+			SELECT 
+				fa.[Id] AS AnswerId,
+				fa.[AnswerTitle] AS AnswerTitle,
+				fa.[Description] AS AnswerDescription,		
+				[dbo].[fn_GetUserJSON](fa.AuthorId) AS AnswersAuthorId,
+				fa.[DateCreated] AS AnswerDateCreated,
+				fa.[ThreadId] AS AnswerThreadId
+			FROM [dbo].[ForumAnswers] AS fa
+			WHERE fa.[ThreadId] = f.[Id]
+			FOR JSON AUTO
+		) AS Answers 
+
+	FROM [dbo].[Forum] AS f
+	INNER JOIN [dbo].[ForumCategories] AS fc
+	ON f.[ForumCategoryId] = fc.[Id]
+	WHERE f.[Id] = @Id;
 
 END
