bddcb906332cf30684db6fb9179aa47ea7152e78
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:26:25 -0500

    Update Forum_Update.sql

--- SQL/Forum_Update.sql
+++ SQL/Forum_Update.sql
@@ -1,29 +1,29 @@
 ALTER proc [dbo].[Forum_Update]
-			@Name nvarchar(4000)
-			,@Description nvarchar(4000)
-			,@ForumCategoryId int
-			,@IsPrivate bit
-		
-			,@Id int 
+		@Name nvarchar(4000)
+		,@Description nvarchar(4000)
+		,@ForumCategoryId int
+		,@IsPrivate bit
+	
+		,@Id int 
 
 /* ------------ TEST CODE ----------------
 DECLARE @Name nvarchar(4000) = 'Bob Smith'
-			  ,@Description nvarchar(4000) = ''
-			  ,@ForumCategoryId int = 2
-			  ,@IsPrivate bit = 1
-			
-			  ,@Id int = 1
+	,@Description nvarchar(4000) = ''
+	,@ForumCategoryId int = 2
+	,@IsPrivate bit = 1
+	
+	,@Id int = 1
 
 EXECUTE [dbo].[Forum_Update]
-			  @Name 
-			  ,@Description 
-			  ,@ForumCategoryId 
-			  ,@IsPrivate 
-			  ,@IsClosed 
-			  ,@DateCreated 
-			  ,@DateAdded 
-
-			  ,@Id 
+	  @Name 
+	  ,@Description 
+	  ,@ForumCategoryId 
+	  ,@IsPrivate 
+	  ,@IsClosed 
+	  ,@DateCreated 
+	  ,@DateAdded 
+	
+	  ,@Id 
 
 Select * from [dbo].[Forum]
 */ -----------END TEST CODE---------------
@@ -31,13 +31,13 @@ as
 
 BEGIN
 
-			UPDATE [dbo].[Forum]
-         SET 
+	UPDATE [dbo].[Forum]
+        SET 
           [Name] = @Name
           ,[Description] = @Description
           ,[ForumCategoryId] = @ForumCategoryId
           ,[IsPrivate] = @IsPrivate
      
-			 WHERE [Id] = @Id
+	WHERE [Id] = @Id
 
  END
