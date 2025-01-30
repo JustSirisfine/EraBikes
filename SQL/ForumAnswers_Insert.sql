1bbc676eb9c3764e3d5b5fc99a1b1f3c216a62af
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:29:37 -0500

    Update ForumAnswers_Insert.sql

--- SQL/ForumAnswers_Insert.sql
+++ SQL/ForumAnswers_Insert.sql
@@ -1,18 +1,18 @@
 ALTER proc [dbo].[ForumAnswers_Insert]
-					@AnswerTitle nvarchar(500),
-					@AnswerDescription nvarchar(MAX),
-					@AnswerAuthorId int,
-					@AnswerThreadId int,
+		@AnswerTitle nvarchar(500)
+		,@AnswerDescription nvarchar(MAX)
+		,@AnswerAuthorId int
+		,@AnswerThreadId int
 
-					@Id int OUTPUT
+		,@Id int OUTPUT
 
 /* ------------- TEST CODE --------------------
 DECLARE @AnswerTitle nvarchar(500) = 'The Title of a Comment/Answer',
-		@Description nvarchar(MAX)= 'The description of a comment/answer, ideally where the commenting/answering will take place',
-		@AuthorId int = 1,
-		@ThreadId int = 1,
+	,@Description nvarchar(MAX)= 'The description of a comment/answer, ideally where the commenting/answering will take place'
+	,@AuthorId int = 1
+	,@ThreadId int = 1
 
-		@Id int OUTPUT
+	,@Id int OUTPUT
 
 EXECUTE [dbo].[ForumAnswers_Insert]
 		@AnswerTitle
@@ -33,13 +33,13 @@ BEGIN
 			   ,[Description]
 			   ,[AuthorId]
 			   ,[ThreadId])
-		 VALUES
-			   (
-			   @AnswerTitle
-			   ,@AnswerDescription
-			   ,@AnswerAuthorId
-			   ,@AnswerThreadId
-			   )
-			   SET @Id = SCOPE_IDENTITY()
+	 VALUES
+		   (
+		   @AnswerTitle
+		   ,@AnswerDescription
+		   ,@AnswerAuthorId
+		   ,@AnswerThreadId
+		   )
+		   SET @Id = SCOPE_IDENTITY()
 
 END
