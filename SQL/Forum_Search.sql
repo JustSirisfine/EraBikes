ed9aea41e1c3f26948cbbece2fab12045a240801
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:30:33 -0500

    Update Forum_Search.sql

--- SQL/Forum_Search.sql
+++ SQL/Forum_Search.sql
@@ -1,45 +1,45 @@
 ALTER PROC [dbo].[Forum_Search]
-			  @Query nvarchar(50)
-			  ,@PageIndex int
-			  ,@PageSize int
+	  @Query nvarchar(50)
+	  ,@PageIndex int
+	  ,@PageSize int
 
 /* ----------------- TEST CODE ------------------
 Declare @Query nvarchar(50)= 'shou'
-		    ,@PageIndex int = 0
-		    ,@PageSize int = 5
+	,@PageIndex int = 0
+	,@PageSize int = 5
 
 execute [dbo].[Forum_Search]
-		    @Query
-		    ,@PageIndex 
-		    ,@PageSize 
+	@Query
+	,@PageIndex 
+	,@PageSize 
 -----------------END TEST CODE ------------------*/
 as
 
 BEGIN
 
-		DECLARE @Rows int = @PageIndex * @PageSize
+	DECLARE @Rows int = @PageIndex * @PageSize
 
-				SELECT f.[Id]
-				   ,f.[Name]
-				   ,f.[Description]
-				   ,f.[ForumCategoryId]
-				   ,fc.[Name]
-				   ,f.[IsPrivate]
-				   ,f.[IsClosed]
-				   ,f.[DateCreated]
-				   ,f.[DateAdded] 
-				   ,dbo.fn_GetUserJSON(f.[CreatedBy]) as [CreatedBy]
+			SELECT f.[Id]
+			   ,f.[Name]
+			   ,f.[Description]
+			   ,f.[ForumCategoryId]
+			   ,fc.[Name]
+			   ,f.[IsPrivate]
+			   ,f.[IsClosed]
+			   ,f.[DateCreated]
+			   ,f.[DateAdded] 
+			   ,dbo.fn_GetUserJSON(f.[CreatedBy]) as [CreatedBy]
 
-				   ,TotalCount = COUNT (1) over ()
+			   ,TotalCount = COUNT (1) over ()
 
-			  FROM [dbo].[Forum] as f
-			  Inner Join [dbo].[ForumCategories] as fc
-			  ON f.ForumCategoryId = fc.Id
-			  WHERE (f.[Name] LIKE '%' + @Query + '%' OR f.[Description] LIKE '%' + @Query + '%' )
+		  FROM [dbo].[Forum] as f
+		  Inner Join [dbo].[ForumCategories] as fc
+		  ON f.ForumCategoryId = fc.Id
+		  WHERE (f.[Name] LIKE '%' + @Query + '%' OR f.[Description] LIKE '%' + @Query + '%' )
 
-			  ORDER BY f.[DateCreated] desc
+		  ORDER BY f.[DateCreated] desc
 
-			  OFFSET @Rows ROWS
-			  FETCH NEXT @PageSize ROWS only
+		  OFFSET @Rows ROWS
+		  FETCH NEXT @PageSize ROWS only
 
 END
