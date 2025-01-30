b98db3e769c537f88853acb8c659628ef5058305
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:06:19 -0500

    Create Forum_Delete.sql

---
+++ SQL/Forum_Delete.sql
@@ -0,0 +1,20 @@
+ALTER proc [dbo].[Forum_Delete]
+					@Id int
+
+/* ------------- TEST CODE -----------
+select* from [dbo].[Forum]
+
+Declare @Id int = 3
+
+Execute [dbo].[Forum_Delete]
+		@Id
+
+select* from [dbo].[Forum]
+*/ ------------END TEST CODE----------
+
+as
+
+BEGIN
+			DELETE FROM [dbo].[Forum]
+			WHERE [Id] = @Id
+END
