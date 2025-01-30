bffc92b0f7795a793f6dec466dc6b4f0e0cc1edb
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 12:50:11 -0500

    Update Forum.cs

--- .NET/Forum.cs
+++ .NET/Forum.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Domain.Forum
+public class Forum
 {
-    public class Forum
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public LookUp ForumCategoryId { get; set; }
-        public bool IsPrivate { get; set; }
-        public bool IsClosed { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateModified { get; set; }
-        public BaseUser AuthorInfo { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public LookUp ForumCategoryId { get; set; }
+    public bool IsPrivate { get; set; }
+    public bool IsClosed { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateModified { get; set; }
+    public BaseUser AuthorInfo { get; set; }
 }
