8af48cc072e197c5bb6702c99a63b27a72546a97
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:19:13 -0500

    Create Forum_SelectPaginated.sql

---
+++ SQL/Forum_SelectPaginated.sql
@@ -0,0 +1,42 @@
+ALTER proc [dbo].[Forum_SelectPaginated]
+					@PageIndex int
+					,@PageSize int
+
+/* -------------------- TEST CODE -------------------
+
+DECLARE @PageIndex int = 0
+		    ,@PageSize int = 10
+
+EXECUTE [dbo].[Forum_SelectPaginated]
+	    	@PageIndex
+		    ,@PageSize
+
+*/ -------------------END TEST CODE------------------
+as
+
+BEGIN
+	
+			DECLARE @Rows int = @PageIndex * @PageSize
+
+			SELECT f.[Id]
+				   ,f.[Name]
+				   ,f.[Description]
+				   ,f.[ForumCategoryId]
+				   ,fc.[Name]
+				   ,f.[IsPrivate]
+				   ,f.[IsClosed]
+				   ,f.[DateCreated]
+				   ,f.[DateAdded] 
+
+				   ,TotalCount = COUNT (1) over ()
+
+			FROM [dbo].[Forum] as f
+			Inner Join [dbo].[ForumCategories] as fc
+			on f.[ForumCategoryId] = fc.[Id]
+
+			ORDER BY f.[DateCreated] DESC
+
+			OFFSET @Rows ROWS
+			FETCH NEXT @PageSize ROWS only 
+
+END
