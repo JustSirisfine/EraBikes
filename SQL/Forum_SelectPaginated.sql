b8a0311e529e6fa0919e67d59e126bb8ea4459c0
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:28:02 -0500

    Update Forum_SelectPaginated.sql

--- SQL/Forum_SelectPaginated.sql
+++ SQL/Forum_SelectPaginated.sql
@@ -1,42 +1,42 @@
 ALTER proc [dbo].[Forum_SelectPaginated]
-					@PageIndex int
-					,@PageSize int
+		@PageIndex int
+		,@PageSize int
 
 /* -------------------- TEST CODE -------------------
 
 DECLARE @PageIndex int = 0
-		    ,@PageSize int = 10
+	,@PageSize int = 10
 
 EXECUTE [dbo].[Forum_SelectPaginated]
-	    	@PageIndex
-		    ,@PageSize
+	@PageIndex
+	,@PageSize
 
 */ -------------------END TEST CODE------------------
 as
 
 BEGIN
 	
-			DECLARE @Rows int = @PageIndex * @PageSize
+		DECLARE @Rows int = @PageIndex * @PageSize
 
-			SELECT f.[Id]
-				   ,f.[Name]
-				   ,f.[Description]
-				   ,f.[ForumCategoryId]
-				   ,fc.[Name]
-				   ,f.[IsPrivate]
-				   ,f.[IsClosed]
-				   ,f.[DateCreated]
-				   ,f.[DateAdded] 
+		SELECT f.[Id]
+			   ,f.[Name]
+			   ,f.[Description]
+			   ,f.[ForumCategoryId]
+			   ,fc.[Name]
+			   ,f.[IsPrivate]
+			   ,f.[IsClosed]
+			   ,f.[DateCreated]
+			   ,f.[DateAdded] 
 
-				   ,TotalCount = COUNT (1) over ()
+			   ,TotalCount = COUNT (1) over ()
 
-			FROM [dbo].[Forum] as f
-			Inner Join [dbo].[ForumCategories] as fc
-			on f.[ForumCategoryId] = fc.[Id]
+		FROM [dbo].[Forum] as f
+		Inner Join [dbo].[ForumCategories] as fc
+		on f.[ForumCategoryId] = fc.[Id]
 
-			ORDER BY f.[DateCreated] DESC
+		ORDER BY f.[DateCreated] DESC
 
-			OFFSET @Rows ROWS
-			FETCH NEXT @PageSize ROWS only 
+		OFFSET @Rows ROWS
+		FETCH NEXT @PageSize ROWS only 
 
 END
