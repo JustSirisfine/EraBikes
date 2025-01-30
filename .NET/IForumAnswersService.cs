af0ff828eabace93a671f4853f7c74f44c9b2ac2
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 12:31:47 -0500

    Update IForumAnswersService.cs

--- .NET/IForumAnswersService.cs
+++ .NET/IForumAnswersService.cs
@@ -1,16 +1,5 @@
-﻿using Sabio.Models.Domain.Forum;
-using Sabio.Models.Requests.Forum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Services.Interfaces
+public interface IForumAnswersService
 {
-    public interface IForumAnswersService
-    {
-        int AddAnswer(ForumAnswerAddRequest model, int userId);
-        ForumAnswers GetForumWithAnswersById(int forumId);
-    }
+    int AddAnswer(ForumAnswerAddRequest model, int userId);
+    ForumAnswers GetForumWithAnswersById(int forumId);
 }
