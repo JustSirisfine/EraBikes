81dbb815ed06ab3d6546b709e87a4a33a0493b88
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:04:09 -0500

    Update and rename Forum_SelectByCategoryPaginated to Forum_SelectByCategoryPaginated.sql

---
+++ SQL/Forum_SelectByCategoryPaginated.sql
@@ -0,0 +1,43 @@
+ALTER proc [dbo].[Forum_SelectByCategoryPaginated]
+				@ForumCategoryId int
+				,@PageIndex int
+				,@PageSize int
+
+as
+/* ---------- TEST CODE ---------------
+Declare @ForumCategoryId int = 1
+	,@PageIndex int = 0
+	,@PageSize int = 27
+
+EXECUTE [dbo].[Forum_SelectByCategoryPaginated]
+	@ForumCategoryId,
+	@PageIndex,
+	@PageSize
+
+*/ ---------END TEST CODE--------------
+
+BEGIN
+		DECLARE @Rows int = @PageIndex * @PageSize
+
+			SELECT f.[Id]
+				,f.[Name]
+				,f.[Description]
+				,f.[ForumCategoryId]
+				,fc.[Name]
+				,f.[IsPrivate]
+				,f.[IsClosed]
+				,f.[DateCreated]
+				,f.[DateAdded] 
+				,dbo.fn_GetUserJSON(f.[CreatedBy]) as [CreatedBy]
+				,TotalCount = COUNT (1) over ()
+
+			FROM [dbo].[Forum] as f
+			Inner Join [dbo].[ForumCategories] as fc
+			ON f.ForumCategoryId = fc.Id
+			WHERE fc.Id = @ForumCategoryId
+	
+			ORDER BY f.[DateCreated] desc
+
+			OFFSET @Rows ROWS
+			FETCH NEXT @PageSize ROWS only
+END
