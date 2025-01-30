8c153a19dec73a7acc9ec9317b765b9813959b7d
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:27:28 -0500

    Update Forum_SelectById.sql

--- SQL/Forum_SelectById.sql
+++ SQL/Forum_SelectById.sql
@@ -1,31 +1,31 @@
 ALTER proc [dbo].[Forum_SelectById]
-					@Id int
+		@Id int
 
 /* ---------------- TEST CODE ------------
 Declare @Id int = 7
 
 EXECUTE [dbo].[Forum_SelectById]
-		    @Id
+	@Id
 
 */ ---------------END TEST CODE-----------
 as
 
 BEGIN
 
-			SELECT f.[Id]
-				   ,f.[Name]
-				   ,f.[Description]
-				   ,f.[ForumCategoryId]
-				   ,fc.[Name]
-				   ,f.[IsPrivate]
-				   ,f.[IsClosed]
-				   ,f.[DateCreated]
-				   ,f.[DateAdded] 
+		SELECT f.[Id]
+			   ,f.[Name]
+			   ,f.[Description]
+			   ,f.[ForumCategoryId]
+			   ,fc.[Name]
+			   ,f.[IsPrivate]
+			   ,f.[IsClosed]
+			   ,f.[DateCreated]
+			   ,f.[DateAdded] 
 
-			  FROM [dbo].[Forum] as f 
-			  Inner Join [dbo].[ForumCategories] as fc
-			  ON f.[ForumCategoryId] = fc.[Id]
+		  FROM [dbo].[Forum] as f 
+		  Inner Join [dbo].[ForumCategories] as fc
+		  ON f.[ForumCategoryId] = fc.[Id]
 
-			  WHERE f.[Id] = @Id
+		  WHERE f.[Id] = @Id
 
 END
