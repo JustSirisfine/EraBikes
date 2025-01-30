17e79e7a20e36cdff2f46cf8751d572ca07a0805
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:13:02 -0500

    Create Forum_SelectById.sql

---
+++ SQL/Forum_SelectById.sql
@@ -0,0 +1,31 @@
+ALTER proc [dbo].[Forum_SelectById]
+					@Id int
+
+/* ---------------- TEST CODE ------------
+Declare @Id int = 7
+
+EXECUTE [dbo].[Forum_SelectById]
+		    @Id
+
+*/ ---------------END TEST CODE-----------
+as
+
+BEGIN
+
+			SELECT f.[Id]
+				   ,f.[Name]
+				   ,f.[Description]
+				   ,f.[ForumCategoryId]
+				   ,fc.[Name]
+				   ,f.[IsPrivate]
+				   ,f.[IsClosed]
+				   ,f.[DateCreated]
+				   ,f.[DateAdded] 
+
+			  FROM [dbo].[Forum] as f 
+			  Inner Join [dbo].[ForumCategories] as fc
+			  ON f.[ForumCategoryId] = fc.[Id]
+
+			  WHERE f.[Id] = @Id
+
+END
