d2fff4430c808532690ce6c3b09a5e7aa4d8e0f2
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 12:32:16 -0500

    Update ForumUpdateRequest.cs

--- .NET/ForumUpdateRequest.cs
+++ .NET/ForumUpdateRequest.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.Forum
+public class ForumUpdateRequest : ForumAddRequest, IModelIdentifier
 {
-    public class ForumUpdateRequest : ForumAddRequest, IModelIdentifier
-    {
-        public int Id { get; set; }
-    }
+    public int Id { get; set; }
 }
+
