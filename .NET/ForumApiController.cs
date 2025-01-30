5c2eb7246e40336db8f6ab08f387ec4982586399
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 12:45:00 -0500

    Update ForumApiController.cs

--- .NET/ForumApiController.cs
+++ .NET/ForumApiController.cs
@@ -1,213 +1,183 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Sabio.Models.Requests.Forum;
-using Sabio.Services;
-using Sabio.Services.Interfaces;
-using Sabio.Web.Controllers;
-using Sabio.Web.Models.Responses;
-using System;
-using Microsoft.AspNetCore.Authorization;
-using Sabio.Models.Interfaces;
-using Sabio.Models;
-using Sabio.Models.Domain.Forum;
-using Sabio.Models.Domain.Blogs;
-
-namespace Sabio.Web.Api.Controllers
+[Route("api/forum")]
+[ApiController]
+public class ForumApiController : BaseApiController
 {
-    [Route("api/forum")]
-    [ApiController]
-    public class ForumApiController : BaseApiController
+    private ILogger _logger;
+    private IForumService _forumService;
+    private IAuthenticationService<int> _authenticationService;
+
+    public ForumApiController(ILogger<ForumApiController> logger,
+        IAuthenticationService<int> authenticationService,
+
+        (IForumService forumService) : base(logger)
     {
-        private ILogger _logger;
-        private IForumService _forumService;
-        private IAuthenticationService<int> _authenticationService;
+        _authenticationService = authenticationService;
+        _logger = logger;
+        _forumService = forumService;
+    }
 
-        public ForumApiController(ILogger<ForumApiController> logger,
-            IAuthenticationService<int> authenticationService,
-
-            IForumService forumService) : base(logger)
+    [HttpPost] 
+    public ActionResult Create(ForumAddRequest model)
+    {
+        ObjectResult result = null;
+        int forumId = _authenticationService.GetCurrentUserId();
+        try
         {
-            _authenticationService = authenticationService;
-            _logger = logger;
-            _forumService = forumService;
+            int id = _forumService.Add(model, forumId);
+            ItemResponse<int> response = new ItemResponse<int>() { Item = id };
+            result = Created201(response);
         }
-
-        [HttpPost]
-        
-        public ActionResult Create(ForumAddRequest model)
+        catch (Exception ex)
         {
-            ObjectResult result = null;
-            int forumId = _authenticationService.GetCurrentUserId();
-            try
-            {
-                int id = _forumService.Add(model, forumId);
-                ItemResponse<int> response = new ItemResponse<int>() { Item = id };
-                result = Created201(response);
-            }
-            catch (Exception ex)
-            {
-                base.Logger.LogError(ex.ToString());
-                ErrorResponse response = new ErrorResponse(ex.Message);
-                result = StatusCode(500, response);
-            }
-            return result;
+            base.Logger.LogError(ex.ToString());
+            ErrorResponse response = new ErrorResponse(ex.Message);
+            result = StatusCode(500, response);
         }
+        return result;
+    }
 
-        [HttpPut("{id:int}")]
-
-        public ActionResult Update(ForumUpdateRequest model)
+    [HttpPut("{id:int}")]
+    public ActionResult Update(ForumUpdateRequest model)
+    {
+        int code = 200;
+        BaseResponse response = null;
+        try
         {
-            int code = 200;
-            BaseResponse response = null;
-
-            try
-            {
-                _forumService.Update(model);
-                response = new SuccessResponse();
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-            }
-            return StatusCode(code, response);
+            _forumService.Update(model);
+            response = new SuccessResponse();
         }
-
-        [HttpDelete("{id:int}")]
-        public ActionResult Delete(int forumId)
+        catch (Exception ex)
         {
-            int code = 200;
-            BaseResponse response = null;
-
-            try
-            {
-                _forumService.Delete(forumId);
-                response = new SuccessResponse();
-            }
-            catch(Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-            }
-            return StatusCode(code, response);
+            code = 500;
+            response = new ErrorResponse(ex.Message);
         }
+        return StatusCode(code, response);
+    }
 
-        [HttpGet("paginate")]
-        [AllowAnonymous]
-        public ActionResult<ItemResponse<Paged<Forum>>> Pagination(int pageIndex, int pageSize)
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int forumId)
+    {
+        int code = 200;
+        BaseResponse response = null;
+        try
         {
-            ActionResult result;
-            try
-            {
-                Paged<Forum> paged = _forumService.Pagination(pageIndex, pageSize);
-                if (paged == null)
-                {
-                    result = NotFound404(new ErrorResponse("Records Not Found"));
-                }
-                else
-                {
-                    ItemResponse<Paged<Forum>> response = new ItemResponse<Paged<Forum>>();
-                    response.Item = paged;
-                    result = Ok200(response);
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex.ToString());
-                result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
-            }
-            return result;
-
+            _forumService.Delete(forumId);
+            response = new SuccessResponse();
         }
-        [HttpGet("search/paginate")]
-        [AllowAnonymous]
-        public ActionResult<ItemResponse<Paged<Forum>>> SearchPaginate(string query, int pageIndex, int pageSize)
+        catch(Exception ex)
         {
-            ActionResult result;
-            try
-            {
-                Paged<Forum> paged = _forumService.SearchPaginate(query, pageIndex, pageSize);
-                if (paged == null)
-                {
-                    result = NotFound404(new ErrorResponse("Records Not Found"));
-                }
-                else
-                {
-                    ItemResponse<Paged<Forum>> response = new ItemResponse<Paged<Forum>>();
-                    response.Item = paged;
-                    result = Ok200(response);
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex.ToString());
-                result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
-            }
-            return result;
-
+            code = 500;
+            response = new ErrorResponse(ex.Message);
         }
+        return StatusCode(code, response);
+    }
 
-        [HttpGet("paginate/bycategory")]
-        public ActionResult<ItemResponse<Paged<Forum>>> SelectByCategory(int forumCategoryId, int pageIndex, int pageSize)
+    [HttpGet("paginate")]
+    [AllowAnonymous]
+    public ActionResult<ItemResponse<Paged<Forum>>> Pagination(int pageIndex, int pageSize)
+    {
+        ActionResult result;
+        try
         {
-            ActionResult result = null;
-            try
+            Paged<Forum> paged = _forumService.Pagination(pageIndex, pageSize);
+            if (paged == null)
             {
-
-                Paged<Forum> paged = _forumService.SelectByCategory(forumCategoryId, pageIndex, pageSize);
-                if (paged == null)
-                {
-                    result = NotFound404(new ErrorResponse("Records Not Found"));
-                }
-                else
-                {
-                    ItemResponse<Paged<Forum>> response = new ItemResponse<Paged<Forum>>();
-                    response.Item = paged;
-                    result = Ok200(response);
-                }
+                result = NotFound404(new ErrorResponse("Records Not Found"));
             }
-            catch (Exception ex)
+            else
             {
-                Logger.LogError(ex.ToString());
-                result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
+                ItemResponse<Paged<Forum>> response = new ItemResponse<Paged<Forum>>();
+                response.Item = paged;
+                result = Ok200(response);
             }
-            return result;
-
         }
-
-        [HttpGet("{id:int}")]
-        [AllowAnonymous]
-        public ActionResult<ItemResponse<Forum>> GetById(int id)
+        catch (Exception ex)
         {
-            int code = 200;
-            BaseResponse response = null;
-            
-            try
-            {
-                Forum aForum = _forumService.GetById(id);
-
-                if (aForum == null)
-                {
-                    code = 404;
-                    response = new ErrorResponse("Application Resource not found.");
-                }
-                else
-                {
-                    response = new ItemResponse<Forum> { Item = aForum };
-                }
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                base.Logger.LogError(ex.ToString());
-                response = new ErrorResponse($"Generic Error: {ex.Message}");
-            }
-
-            return StatusCode(code, response);
-
+            Logger.LogError(ex.ToString());
+            result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
         }
+        return result;
+    }
+    
+    [HttpGet("search/paginate")]
+    [AllowAnonymous]
+    public ActionResult<ItemResponse<Paged<Forum>>> SearchPaginate(string query, int pageIndex, int pageSize)
+    {
+        ActionResult result;
+        try
+        {
+            Paged<Forum> paged = _forumService.SearchPaginate(query, pageIndex, pageSize);
+            if (paged == null)
+            {
+                result = NotFound404(new ErrorResponse("Records Not Found"));
+            }
+            else
+            {
+                ItemResponse<Paged<Forum>> response = new ItemResponse<Paged<Forum>>();
+                response.Item = paged;
+                result = Ok200(response);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex.ToString());
+            result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
+        }
+        return result;
+    }
 
+    [HttpGet("paginate/bycategory")]
+    public ActionResult<ItemResponse<Paged<Forum>>> SelectByCategory(int forumCategoryId, int pageIndex, int pageSize)
+    {
+        ActionResult result = null;
+        try
+        {
+            Paged<Forum> paged = _forumService.SelectByCategory(forumCategoryId, pageIndex, pageSize);
+            if (paged == null)
+            {
+                result = NotFound404(new ErrorResponse("Records Not Found"));
+            }
+            else
+            {
+                ItemResponse<Paged<Forum>> response = new ItemResponse<Paged<Forum>>();
+                response.Item = paged;
+                result = Ok200(response);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex.ToString());
+            result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
+        }
+        return result;
+    }
+
+    [HttpGet("{id:int}")]
+    [AllowAnonymous]
+    public ActionResult<ItemResponse<Forum>> GetById(int id)
+    {
+        int code = 200;
+        BaseResponse response = null;
+        try
+        {
+            Forum aForum = _forumService.GetById(id);
+
+            if (aForum == null)
+            {
+                code = 404;
+                response = new ErrorResponse("Application Resource not found.");
+            }
+            else
+            {
+                response = new ItemResponse<Forum> { Item = aForum };
+            }
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            base.Logger.LogError(ex.ToString());
+            response = new ErrorResponse($"Generic Error: {ex.Message}");
+        }
+        return StatusCode(code, response);
     }
 }
