499d003e2e6793e289cd993e6b69367408a327c1
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:30:50 -0500

    Update Forum_SelectByCategoryPaginated.sql

--- SQL/Forum_SelectByCategoryPaginated.sql
+++ SQL/Forum_SelectByCategoryPaginated.sql
@@ -1,7 +1,7 @@
 ALTER proc [dbo].[Forum_SelectByCategoryPaginated]
-				@ForumCategoryId int
-				,@PageIndex int
-				,@PageSize int
+		@ForumCategoryId int
+		,@PageIndex int
+		,@PageSize int
 
 as
 /* ---------- TEST CODE ---------------
@@ -17,27 +17,27 @@ EXECUTE [dbo].[Forum_SelectByCategoryPaginated]
 */ ---------END TEST CODE--------------
 
 BEGIN
-		DECLARE @Rows int = @PageIndex * @PageSize
+	DECLARE @Rows int = @PageIndex * @PageSize
 
-			SELECT f.[Id]
-				,f.[Name]
-				,f.[Description]
-				,f.[ForumCategoryId]
-				,fc.[Name]
-				,f.[IsPrivate]
-				,f.[IsClosed]
-				,f.[DateCreated]
-				,f.[DateAdded] 
-				,dbo.fn_GetUserJSON(f.[CreatedBy]) as [CreatedBy]
-				,TotalCount = COUNT (1) over ()
+		SELECT f.[Id]
+			,f.[Name]
+			,f.[Description]
+			,f.[ForumCategoryId]
+			,fc.[Name]
+			,f.[IsPrivate]
+			,f.[IsClosed]
+			,f.[DateCreated]
+			,f.[DateAdded] 
+			,dbo.fn_GetUserJSON(f.[CreatedBy]) as [CreatedBy]
+			,TotalCount = COUNT (1) over ()
 
-			FROM [dbo].[Forum] as f
-			Inner Join [dbo].[ForumCategories] as fc
-			ON f.ForumCategoryId = fc.Id
-			WHERE fc.Id = @ForumCategoryId
-	
-			ORDER BY f.[DateCreated] desc
+		FROM [dbo].[Forum] as f
+		Inner Join [dbo].[ForumCategories] as fc
+		ON f.ForumCategoryId = fc.Id
+		WHERE fc.Id = @ForumCategoryId
 
-			OFFSET @Rows ROWS
-			FETCH NEXT @PageSize ROWS only
+		ORDER BY f.[DateCreated] desc
+
+		OFFSET @Rows ROWS
+		FETCH NEXT @PageSize ROWS only
 END
