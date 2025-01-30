a9dd5d9f749c015242c81af150c76677c20a717b
Sierra Meccariello <163929593+JustSirisfine@users.noreply.github.com>
2025-01-30 13:08:40 -0500

    Create Forum_Insert.sql

---
+++ SQL/Forum_Insert.sql
@@ -0,0 +1,51 @@
+ALTER proc [dbo].[Forum_Insert]
+					@Name nvarchar(4000)
+					,@Description nvarchar(4000)
+					,@ForumCategoryId int
+					,@AuthorId int
+					,@IsPrivate bit
+
+					,@Id int OUTPUT
+
+/* ------------ TEST CODE ------------
+Declare @Name nvarchar(4000) = ''
+		,@Description nvarchar(4000) = ''
+		,@ForumCategoryId int = 1
+		,@IsPrivate bit = 0
+
+		,@Id int OUTPUT
+
+Execute [dbo].[Forum_Insert]
+		@Name 
+		,@Description 
+		,@ForumCategoryId 
+		,@IsPrivate 
+
+		,@Id int OUTPUT
+		
+select * from [dbo].[Forum]
+*/-----------END TEST CODE -----------
+as
+
+BEGIN
+
+		INSERT INTO [dbo].[Forum]
+      (
+         [Name]
+         ,[Description]
+         ,[ForumCategoryId]
+         ,[CreatedBy]
+         ,[IsPrivate]
+		  )
+		VALUES
+      (
+		    @Name 
+			  ,@Description 
+			  ,@ForumCategoryId 
+			  ,@AuthorId
+			  ,@IsPrivate 
+			)
+
+			SET @Id = SCOPE_IDENTITY()
+
+END
